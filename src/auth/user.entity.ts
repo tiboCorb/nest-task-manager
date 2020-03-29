8f13ffd9d1638489c71db4739f1c507008e13f33
tiboCorb <corbrionthibault@gmail.com>
2020-03-29 22:58:40 +0200

    implement signUp

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,8 +1,9 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique } from "typeorm"
 
 @Entity()
+@Unique(['username'])
 export class User extends BaseEntity {
-    
+
     @PrimaryGeneratedColumn()
     id: number;
 
@@ -11,4 +12,7 @@ export class User extends BaseEntity {
 
     @Column()
     password: string;
+
+    @Column()
+    salt: string;
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,32 @@
 import { Repository, EntityRepository } from "typeorm"
 import { User } from "./user.entity"
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto"
+import { ConflictException } from "@nestjs/common";
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
 
+    async singUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+
+        const { username, password } = authCredentialsDto;
+        const user = new User();
+
+        user.salt = await bcrypt.genSalt();
+        user.username = username;
+        user.password = await this.hashPassword(password, user.salt );
+
+        try {
+            await user.save();
+        } catch (error) {
+            if (error.code === '23505') { // duplicate user name
+                throw new ConflictException('Username already use')
+            }
+        }
+
+    }
+
+    private async hashPassword(password: string, salt: string): Promise<string> {
+        return await bcrypt.hash(password, salt);
+    }
 }
